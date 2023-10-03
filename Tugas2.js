8140f9400d07320d4a86ba633b999bdb93631600
HikmahHidayat <114964286+HikmahHidayat@users.noreply.github.com>
2023-10-04 01:33:05 +0700

    Update and rename Tugas.js to Tugas2.js

--- Tugas2.js
+++ Tugas2.js
@@ -1,5 +1,5 @@
-const firstName = "Aden";
-const lastName = "Rifalgi Rustam";
+const firstName = "Hikmah";
+const lastName = "Hidayat";
 const age = 20;
 const isMarried = false;
 console.log(firstName);
